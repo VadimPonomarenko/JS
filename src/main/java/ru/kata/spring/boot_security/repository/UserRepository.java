2f9db99a614304c35e3e4294418bf2c24154f7e3
VadimPonomarenko <lastlight0120@gmail.com>
2022-08-25 22:04:46 +0300

    first commit

---
+++ src/main/java/ru/kata/spring/boot_security/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package ru.kata.spring.boot_security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import ru.kata.spring.boot_security.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByEmail(String email);
+
+    @Query("select u FROM User u join fetch u.roles where u.username =:username")
+    User findByUsername(@Param("username") String username);
+//
+    default void merge(User user) {
+        save(user);
+    }
+}
+
